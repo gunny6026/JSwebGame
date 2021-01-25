9cc93d6dfcf60abf1127425f51529e5fc0254fde
gunny6026 <gunny6026@naver.com>
2021-01-25 10:00:50 +0900

    refacroring

--- 4.숫자야구/6.리팩토링과개념복습/refactoring.js
+++ 4.숫자야구/6.리팩토링과개념복습/refactoring.js
@@ -1,23 +1,31 @@
 var 바디 = document.body;
 
-var 숫자후보 = [1,2,3,4,5,6,7,8,9];
+var 숫자후보;
+var 숫자배열;
+var 틀린횟수 
+var 기회 
 
-var 숫자배열 = [];
+function 숫자뽑기(){
+    //숫자야구시작.textContent = "숫자야구를 시작합니다."
+     숫자후보 = [1,2,3,4,5,6,7,8,9];
+        숫자배열 = [];
 
-
-for(var i = 0; i< 4; i++){
-  
-    var 뽑은것 = 숫자후보.splice(Math.floor(Math.random()*9) -i ,1)[0]
-                        
-    숫자배열.push(뽑은것);
-   
-}
-
+    for(var i = 0; i< 4; i++){
+    
+        var 뽑은것 = 숫자후보.splice(Math.floor(Math.random()*9) -i ,1)[0]                   
+        숫자배열.push(뽑은것);
+    }
+    틀린횟수 = 0;
+    기회 = 5;
      console.log(숫자배열);
+}
+숫자뽑기()
+
     
 var 결과 = document.createElement('h1');
 바디.append(결과);
 var 안내문 = document.createElement('p');
+
 
 바디.append(안내문);
 안내문.textContent = "숫자 4자리를 입력하세요"
@@ -37,65 +45,59 @@ var 버튼 = document.createElement('button');
 
 var 값들 = document.createElement("p")
 
+var 틀린횟수카운트 = document.createElement('h3');
+바디.append(틀린횟수카운트);
+
 바디.append(값들);
 
 입력창.type = 'number';
+ var 틀린횟수 = 0;
+ var 기회 = 5;
 폼.addEventListener('submit', function 비동기(event){
     event.preventDefault();
-
-    // var 답1 = Number(입력창.value);
-    // console.log(typeof 답1); // number
-    // console.log(typeof 입력창.value) // 숫자를 입력해도 string 
-
     var 답 = 입력창.value
     if(답 === ""){
         alert("숫자를 입력하세요 ")
     }else if(답.length !== 4){
         alert("숫자 4자리만 입력하세요")
-        
-
-    }else{ 
-                var 틀린횟수 = 0;
+    }else{   
             if(답 === 숫자배열.join('')){  // 값이 같으면
-                결과.textContent = " 홈런";
-              
+                결과.textContent = " 홈런!!!!!!! 축하합니다.";
                 입력창.focus();
                 값들.textContent = "정답: " + 숫자배열.join('');
-                숫자후보 = [1,2,3,4,5,6,7,8,9];
-                숫자배열 = [];
-                for(var i = 0; i< 4; i++){
-    
-                var 뽑은것 = 숫자후보.splice(Math.floor(Math.random()*9) -i ,1)[0]
-                                    
-                숫자배열.push(뽑은것);
-                }
-                console.log(숫자배열);
+                값들.textContent = "새로 게임을 시작하겠습니다."
+                입력창.value= "";
+               숫자뽑기()
                 
                 
-                틀린횟수  = 0;
                 
                                 
-                // 틀린횟수가 올라가지 않음 왜냐하면 var 틀린횟수 =0 선언을
-                // 이벤트 리스터 안에서 했기 때문이다. refactoring.js에서는 구현이 잘됨
+
             }else{          // 값이 틀리면???
              
-                틀린횟수 = 틀린횟수 + 1; 
+                 틀린횟수 += 1;
+                기회 -= 1;
                 var 스트라이크 = 0;
                 var 볼 = 0;
                 var 답배열 = 답.split('');
-                
+               
                 console.log("틀린횟수 : " +틀린횟수)
                 console.log(typeof 틀린횟수)
                 console.log(답배열)
-                if(틀린횟수 > 3){
-                    결과.textContent = "10번 넘게 틀려서 실패 답은 : "+ 숫자배열.join('');
+                if(틀린횟수 >=5){
+                   // 틀린횟수가 5번이 넘어버리면 경고창을 띄우고 새로운 게임을 시작한다.
+                   // 근데 마지막에 
+                        값들.textContent = "입력한 답 : " + 답 ;   
+                        틀린횟수카운트.textContent = "틀린횟수 : " +  틀린횟수;   
+                        값들.textContent = "기회가 총 "+기회 +"번 남았습니다." 
+                    alert( "기회를 다 사용했습니다. 답은 ~ : "+ 숫자배열.join(''))
                     입력창.value = '';
                     입력창.focus();
+                    숫자뽑기()
+                   
                 }else{
                     
-                    for(var i = 0; i< 4; i++){  
-                       
-    
+                    for(var i = 0; i< 4; i++){   
                         if(답배열[i] == 숫자배열[i]){  // 스트라이크
                              스트라이크 += 1;
                              입력창.focus();
@@ -104,22 +106,16 @@ var 값들 = document.createElement("p")
                                 // 볼
                                 볼 += 1;
                             입력창.focus();
-                            
                         }
-                        
-                        
-                        
                         결과.textContent = 스트라이크 +"스트라이크 " + 볼 + " 볼입니다.";
                     }
-                    for(var i=0; i<10; i++){
+                        
+                  } // 틀린횟수 > 10 else
 
-                        값들.textContent = "입력한 답 : " + 답;
-                    }
-                } // 틀린횟수 > 10 else
-                
-            } // 값이 틀리면 else 
+                        값들.textContent = "입력한 답 : " + 답 ;   
+                        틀린횟수카운트.textContent = "틀린횟수 : " +  틀린횟수;   
+                        값들.textContent = "기회가 총 "+기회 +"번 남았습니다." 
 
-       
-
+                } // 값이 틀리면 else 
     }
 })
